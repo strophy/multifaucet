17e5c88fad773d8ab0973fc6a0c1d0a94941af60
strophy <badfunkstripe@gmail.com>
2022-09-08 19:05:19 +1000

    debug: don't seed rng

--- libraries/simple-captcha/simple-php-captcha.php
+++ libraries/simple-captcha/simple-php-captcha.php
@@ -65,7 +65,7 @@ function simple_php_captcha($config = array()) {
 	if( $captcha_config['max_font_size'] < $captcha_config['min_font_size'] ) $captcha_config['max_font_size'] = $captcha_config['min_font_size'];
 	
 	// Use milliseconds instead of seconds
-	srand(microtime() * 100);
+	// srand(microtime() * 100);
 	
 	// Generate CAPTCHA code if not set by user
 	if( empty($captcha_config['code']) ) {
